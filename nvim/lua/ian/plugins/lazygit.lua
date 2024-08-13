875552331e6a3d073d779ab140369a72be8e3466
Ian Mitchell <ian.mitchell@hey.com>
2024-08-13 15:05:45 -0700

    Adds telescope integration

--- nvim/lua/ian/plugins/lazygit.lua
+++ nvim/lua/ian/plugins/lazygit.lua
@@ -1,16 +1,20 @@
 return {
-	"kdheepak/lazygit.nvim",
-	cmd = {
-		"LazyGit",
-		"LazyGitConfig",
-		"LazyGitCurrentFile",
-		"LazyGitFilter",
-		"LazyGitFilterCurrentFile",
-	},
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-	},
-	keys = {
-		{ "<leader>lg", "<cmd>LazyGit<CR>", desc = "Open LazyGit" },
-	},
+  "kdheepak/lazygit.nvim",
+  cmd = {
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile",
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  keys = {
+    { "<leader>lg", "<cmd>LazyGit<CR>", desc = "Open LazyGit" },
+  },
+  config = function()
+    require("telescope").load_extension "lazygit"
+  end,
 }
