24c4a174d8bd98a702cc8d2a854e14991e240d9e
Ian Mitchell <ian.mitchell@hey.com>
2024-08-13 15:53:50 -0700

    Better autocomplete

--- nvim/lua/ian/plugins/nvim-cmp.lua
+++ nvim/lua/ian/plugins/nvim-cmp.lua
@@ -21,7 +21,7 @@ return {
 
     cmp.setup {
       completion = {
-        completeopt = "menu,menuone,preview,noselect",
+        completeopt = "menu,menuone,preview",
       },
       snippet = {
         expand = function(args)
@@ -34,6 +34,19 @@ return {
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
         ["<CR>"] = cmp.mapping.confirm { select = false },
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
+          if cmp.visible() then
+            local entry = cmp.get_selected_entry()
+            if not entry then
+              cmp.select_next_item { behavior = cmp.SelectBehavior.Select }
+            end
+
+            cmp.confirm()
+          else
+            fallback()
+          end
+        end, { "i", "s", "c" }),
       },
 
       sources = cmp.config.sources {
@@ -44,11 +57,41 @@ return {
       },
 
       formatting = {
-        format = lspkind.cmp_format {
-          maxwidth = 50,
-          ellipsis_char = "…",
+        fields = { "kind", "abbr", "menu" },
+        format = function(entry, vim_item)
+          local kind = lspkind.cmp_format {
+            mode = "symbol_text",
+            maxwidth = 50,
+            ellipsis_char = "…",
+          }(entry, vim_item)
+          local strings = vim.split(kind.kind, "%s", { trimempty = true })
+          kind.kind = " " .. (strings[1] or "") .. " "
+          kind.menu = "    (" .. (strings[2] or "") .. ")"
+
+          return kind
+        end,
+      },
+
+      window = {
+        completion = {
+          winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+          col_offset = -3,
+          side_padding = 0,
         },
       },
     }
+
+    -- Solve Tailwind Perf issues: https://github.com/hrsh7th/nvim-cmp/issues/1828
+    -- vim.api.nvim_create_autocmd("LspAttach", {
+    --   group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+    --   callback = function()
+    --     for _, client in pairs((vim.lsp.get_clients {})) do
+    --       if client.name == "tailwindcss" then
+    --         client.server_capabilities.completionProvider.triggerCharacters =
+    --           { '"', "'", "`", ".", "(", "[", "!", "/", ":" }
+    --       end
+    --     end
+    --   end,
+    -- })
   end,
 }
