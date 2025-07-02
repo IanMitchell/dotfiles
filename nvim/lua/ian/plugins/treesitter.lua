ede98c98686c3858b2c45625cc4582ff18b19a5e
Ian Mitchell <ian.mitchell@hey.com>
2025-07-01 21:25:46 -0700

    Formatting

--- nvim/lua/ian/plugins/treesitter.lua
+++ nvim/lua/ian/plugins/treesitter.lua
@@ -97,13 +97,13 @@ return {
     "nvim-treesitter/nvim-treesitter-context",
     dependencies = { "nvim-treesitter/nvim-treesitter" },
     config = function()
-      local tsc = require("treesitter-context")
+      local tsc = require "treesitter-context"
 
-      tsc.setup({
+      tsc.setup {
         enable = false,
         max_lines = 1,
         trim_scope = "inner",
-      })
+      }
     end,
   },
 }
