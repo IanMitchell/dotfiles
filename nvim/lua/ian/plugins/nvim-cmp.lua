45a5ce8fe0a155a261afb2a928ed7f1cb26908fb
Ian Mitchell <ian.mitchell@hey.com>
2025-07-01 21:04:34 -0700

    Remove Wilder

--- nvim/lua/ian/plugins/nvim-cmp.lua
+++ nvim/lua/ian/plugins/nvim-cmp.lua
@@ -5,6 +5,7 @@ return {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
     "hrsh7th/cmp-nvim-lsp-signature-help",
     "saadparwaiz1/cmp_luasnip",
     "onsails/lspkind.nvim",
@@ -96,5 +97,28 @@ return {
         ghost_text = true,
       },
     }
+
+    -- `/` cmdline setup.
+    cmp.setup.cmdline("/", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = "buffer" },
+      },
+    })
+
+    -- `:` cmdline setup.
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = "path" },
+      }, {
+        {
+          name = "cmdline",
+          option = {
+            ignore_cmds = { "Man", "!" },
+          },
+        },
+      }),
+    })
   end,
 }
