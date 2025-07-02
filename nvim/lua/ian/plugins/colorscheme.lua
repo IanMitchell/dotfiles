7722e2d1b2690c19b7f83e006245dfbd32a640d0
Ian Mitchell <ian.mitchell@hey.com>
2025-07-01 21:25:54 -0700

    Better colors

--- nvim/lua/ian/plugins/colorscheme.lua
+++ nvim/lua/ian/plugins/colorscheme.lua
@@ -1,19 +1,32 @@
 return {
-  {
-    -- "navarasu/onedark.nvim",
-    "folke/tokyonight.nvim",
-    priority = 1000,
-    lazy = false,
-    config = function()
-      vim.cmd [[colorscheme tokyonight]]
-      -- local theme = require "onedark"
-      -- theme.setup {
-      --   style = "deep",
-      --   transparent = true,
-      -- }
-      -- theme.load()
+	{
+		"navarasu/onedark.nvim",
+		priority = 1000,
+		lazy = false,
+		cond = function()
+			return vim.o.background == "dark"
+		end,
+		config = function()
+			local theme = require "onedark"
+			theme.setup {
+				style = "darker",
+				transparent = true,
+			}
+			theme.load()
 
-      -- vim.cmd [[colorscheme onedark]]
-    end,
-  },
+			vim.cmd [[colorscheme onedark]]
+		end,
+	},
+	{
+		"sonph/onehalf",
+		priority = 1000,
+		lazy = false,
+		cond = function()
+			return vim.o.background == "light"
+		end,
+		config = function(plugin)
+			vim.opt.rtp:append(plugin.dir .. "/vim")
+			vim.cmd [[colorscheme onehalflight]]
+		end,
+	},
 }
